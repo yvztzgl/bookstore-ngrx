fddc2631b8b1f375183c61966f5bcf378ddfb334
yvztzgl <yavuztzgl@gmail.com>
2024-10-07 20:54:34 +0300

    added new books & style fix

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -16,75 +16,88 @@ import { BookComponent } from "./components/book/book.component";
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit {
-  books:Book[] = [
+  books: Book[] = [
     {
-      id:'1',
-      title:'Çocuk, Köstebek, Tilki ve At',
-      author:'Charlie Mackesy',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:256/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F268597_0c99f_1640083713.jpg',
-      price:100
+        id: '1',
+        title: 'Çocuk, Köstebek, Tilki ve At',
+        author: 'Charlie Mackesy',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:256/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F268597_0c99f_1640083713.jpg',
+        price: 100
     },
     {
-      id:'2',
-      title:'Böyle Buyurdu Zerdüşt',
-      author:'Friedrich Nietzsche',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F2111894_1703967400_4wFai.jpg',
-      price:120
-    },{
-      id:'3',
-      title:'Ermiş',
-      author:'Halil Cibran',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2FErmis_852_1389815786.jpg',
-      price:79
+        id: '2',
+        title: 'Böyle Buyurdu Zerdüşt',
+        author: 'Friedrich Nietzsche',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F2111894_1703967400_4wFai.jpg',
+        price: 120
+    }, {
+        id: '3',
+        title: 'Ermiş',
+        author: 'Halil Cibran',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2FErmis_852_1389815786.jpg',
+        price: 79
     },
     {
-      id:'4',
-      title:'Uçurtmayı Vurmasınlar',
-      author:'Feride Çiçekoğlu',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56586_c5768_1599597955.jpg',
-      price:100
+        id: '4',
+        title: 'Uçurtmayı Vurmasınlar',
+        author: 'Feride Çiçekoğlu',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56586_c5768_1599597955.jpg',
+        price: 100
     },
     {
-      id:'5',
-      title:'Sineklerin Tanrısı',
-      author:'William Golding',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F27823_683b1_1590098323.jpg',
-      price:190
+        id: '5',
+        title: 'Sineklerin Tanrısı',
+        author: 'William Golding',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F27823_683b1_1590098323.jpg',
+        price: 190
     },
     {
-      id:'6',
-      title:'Hayvan Çiftliği',
-      author:'George Orwell',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56085_RXIua_1525561545.jpg',
-      price:140
+        id: '6',
+        title: 'Hayvan Çiftliği',
+        author: 'George Orwell',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56085_RXIua_1525561545.jpg',
+        price: 120
     },
     {
-      id:'7',
-      title:'Otomatik Portakal',
-      author:'Anthony Burgess',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F9414_0e26b_1553711443.jpg',
-      price:140
-    },{
-      id:'8',
-      title:'Açlık',
-      author:'Knut Hamsun',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2F%2Fkitaplar%2F101_1441502227.jpg',
-      price:140
+        id: '7',
+        title: 'Otomatik Portakal',
+        author: 'Anthony Burgess',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F9414_0e26b_1553711443.jpg',
+        price: 135
+    }, {
+        id: '8',
+        title: 'Açlık',
+        author: 'Knut Hamsun',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2F%2Fkitaplar%2F101_1441502227.jpg',
+        price: 58
     },
     {
-      id:'9',
-      title:'İnsanlığımı Yitirirken',
-      author:'Osamu Dazai',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F316900_sCQtI_1649925794.jpg',
-      price:140
-    },{
-      id:'10',
-      title:'Yaşamak',
-      author:'Yu Hua',
-      imgUrl:'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F87436_05203_1570726113.jpg',
-      price:140
+        id: '9',
+        title: 'İnsanlığımı Yitirirken',
+        author: 'Osamu Dazai',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:640/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F316900_sCQtI_1649925794.jpg',
+        price: 90
+    }, {
+        id: '10',
+        title: 'Yaşamak',
+        author: 'Yu Hua',
+        imgUrl: 'https://r2.1k-cdn.com/sig/size:384/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F87436_05203_1570726113.jpg',
+        price: 85
+    }, {
+        id: '11',
+        title: 'Introduction to Algorithms',
+        author: 'Thomas H. Cormen',
+        imgUrl: 'https://m.media-amazon.com/images/I/61O5SsbL8HL._AC_UF1000,1000_QL80_.jpg',
+        price: 160
     }
-  ]
+    , {
+        id: '12',
+        title: 'Head First Java: A Brain-Friendly Guide',
+        author: 'Kathy Sierra',
+        imgUrl: 'https://m.media-amazon.com/images/I/819DmGB3vhL._AC_UF1000,1000_QL80_.jpg',
+        price: 100
+    }
+]
   books$ = this.store.select(selectBasket);
   basketItems:ReadonlyArray<BasketItem> = [];
   constructor(private store:Store){
